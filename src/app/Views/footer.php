26bfb1e764fe525136fb66d2717aedefe4693854
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-04-04 04:29:05 -0400

    Substitute individualized data with environmental variables.

--- src/app/Views/footer.php
+++ src/app/Views/footer.php
@@ -2,10 +2,10 @@
 			</main>
 			<footer class="headerfooter">
 			    <div id="footertext" class="bodytext">
-			        <?= lang('Template.originalContent') ?> &copy; 2009–<?= date('Y') ?> <a href="https://www.markconnelly.pro/<?= \Config\Services::request()->getLocale() ?>/contact/">Mark A. Connelly</a>
+			        <?= lang('Template.originalContent') ?> &copy; <?= getenv('app_compiler_copyright_start') ?>–<?= date('Y') ?> <a href="<?= getenv('app_compiler_url') ?>"><?= getenv('app_compiler_name') ?></a>
 			        <?= lang('Template.license') ?> <a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.<?= \Config\Services::request()->getLocale() ?>">CC BY-SA 4.0</a> |
 			        <a href="/<?= \Config\Services::request()->getLocale() ?>/disclaimer/"><?= lang('Template.disclaimers') ?></a> |
-					<a href="https://opendata.localgeohistory.pro/"><?= lang('Template.opendata') ?></a> |
+					<a href="<?= getenv('app_compiler_opendataurl') ?>"><?= lang('Template.opendata') ?></a> |
 			        <?= ($live ? 'LIVE as of' : 'Data updated') ?> <?= $updated ?>
 			    </div>
 			</footer>
