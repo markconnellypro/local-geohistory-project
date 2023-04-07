6d326771f05261917374d7fc81de4abdb3f25a10
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-04-06 23:03:47 -0400

    Correction to 1.5 -> 1.6 patch

--- inpostgis/postgresql_calendar_extension_update/postgresql_calendar_extension_update_1.5_1.6.sql
+++ inpostgis/postgresql_calendar_extension_update/postgresql_calendar_extension_update_1.5_1.6.sql
@@ -9,7 +9,7 @@ $_pg_tle_$
 -- 1.5 -> 1.6: Correct regnalyeardaterangetext datatype
 --
 
-ALTER TABLE regnalyeartest
+ALTER TABLE calendar.regnalyear
   ALTER COLUMN regnalyeardaterangetext TYPE text;
 
 $_pg_tle_$
