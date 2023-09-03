0805187fa3da117f1089412fe6e212cff84af738
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-03 16:14:18 -0400

    Reorganize base map content

--- src/app/Views/leaflet_state_base.php
+++ src/app/Views/leaflet_state_base.php
@@ -3,17 +3,25 @@ function fixFirefoxPrint() {
 return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 }
 
-var tilehostingHillATT = 'Base map: <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>. Hillshading: <a href="https://aws.amazon.com/public-datasets/terrain/">AWS</a> &copy; <a href="https://github.com/tilezen/joerd/blob/master/docs/attribution.md">Mapzen contributors</a>.';
+var baseMapATT = 'Base map: <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>.';
 
-var baseMap = L.maplibreGL({
-attribution: tilehostingHillATT,
-style: '/en/<?= $state ?>/map-base/',
-preserveDrawingBuffer: fixFirefoxPrint(), // Taken from http://fuzzytolerance.info/blog/2016/07/01/Printing-Mapbox-GL-JS-maps-in-Firefox/
-pane: 'tilePane'
-});
+<?php if (!empty($state)) { ?>
+baseMapATT = baseMapATT + ' Hillshading: <a href="https://aws.amazon.com/public-datasets/terrain/">AWS</a> &copy; <a href="https://github.com/tilezen/joerd/blob/master/docs/attribution.md">Mapzen contributors</a>.';
 
+var baseMapUrl = '/en/<?= $state ?>/map-base/';
 var governmentOverlayMap = L.maplibreGL({
 style: '/en/<?= $state ?>/map-overlay/',
 preserveDrawingBuffer: fixFirefoxPrint(), // Taken from http://fuzzytolerance.info/blog/2016/07/01/Printing-Mapbox-GL-JS-maps-in-Firefox/
 pane: 'overlayPane'
 });
+<?php } else { ?>
+var baseMapUrl = 'https://api.maptiler.com/maps/outdoor-v2/style.json?key=<?= getenv('maptiler_key') ?>';
+<?php } ?>
+
+var baseMap = L.maplibreGL({
+attribution: baseMapATT,
+style: baseMapUrl,
+preserveDrawingBuffer: fixFirefoxPrint(), // Taken from http://fuzzytolerance.info/blog/2016/07/01/Printing-Mapbox-GL-JS-maps-in-Firefox/
+pane: 'tilePane'
+});
+
