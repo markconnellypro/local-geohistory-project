e590b04496c1dda7cc594066bd1c7acc7ee9632e
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-19 00:23:54 -0400

    Add PMTiles support

--- src/app/Views/leaflet_source.php
+++ src/app/Views/leaflet_source.php
@@ -3,3 +3,10 @@
 <script src="/<?= ($online ? '/unpkg.com/maplibre-gl@3.0.0/dist' : 'asset/map') ?>/maplibre-gl.js"></script>
 <script src="/<?= ($online ? '/unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.19' : 'asset/map') ?>/leaflet-maplibre-gl.js"></script>
 <link rel="stylesheet" href="/<?= ($online ? '/unpkg.com/maplibre-gl@3.0.0/dist' : 'asset/map') ?>/maplibre-gl.css" crossorigin="anonymous" />
+<?php if (substr(getenv('map_tile'), 0, 7) == 'pmtiles') { ?>
+<script src="https://unpkg.com/pmtiles@2.10.0/dist/index.js"></script>
+<script>
+    let protocol = new pmtiles.Protocol();
+    maplibregl.addProtocol("pmtiles",protocol.tile);
+</script>
+<?php } ?>
