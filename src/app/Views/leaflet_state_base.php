6b1d11fbaad9fc9086b91b03751e869d73d3381c
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-19 02:00:50 -0400

    Multilingual support

--- src/app/Views/leaflet_state_base.php
+++ src/app/Views/leaflet_state_base.php
@@ -4,11 +4,11 @@ return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 }
 
 var baseMapATT = 'Base map: <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>.  Hillshading: <a href="https://aws.amazon.com/public-datasets/terrain/">AWS</a> &copy; <a href="https://github.com/tilezen/joerd/blob/master/docs/attribution.md">Mapzen contributors</a>.';
-var baseMapUrl = '/en/<?= (empty($state) ? '' : $state . "/") ?>map-base/<?= ($zoom ? "zoom/" : '') ?>';
+var baseMapUrl = '/<?= \Config\Services::request()->getLocale() ?>/<?= (empty($state) ? '' : $state . "/") ?>map-base/<?= ($zoom ? "zoom/" : '') ?>';
 
 <?php if (!empty($state)) { ?>
 var governmentOverlayMap = L.maplibreGL({
-style: '/en/<?= $state ?>/map-overlay/',
+style: '/<?= \Config\Services::request()->getLocale() ?>/<?= $state ?>/map-overlay/',
 preserveDrawingBuffer: fixFirefoxPrint(), // Taken from http://fuzzytolerance.info/blog/2016/07/01/Printing-Mapbox-GL-JS-maps-in-Firefox/
 pane: 'overlayPane'
 });
