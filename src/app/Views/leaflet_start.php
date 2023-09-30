cd2cf7018c13c2f7fdbd29822d070a48ac18665c
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-30 00:19:37 -0400

    Esri Leaflet 3.0.10 (modified) -> 3.0.11

--- src/app/Views/leaflet_start.php
+++ src/app/Views/leaflet_start.php
@@ -6,8 +6,8 @@ if ($needRotation) { /* Version 0.9.3 */ ?>
 <script src="/<?= ($online ? '/api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1' : 'asset/map') ?>/Leaflet.fullscreen.min.js"></script>
 <link rel="stylesheet" href="/<?= ($online ? '/api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1' : 'asset/map') ?>/leaflet.fullscreen.css">
 <script src="//unpkg.com/dom-to-image@2.6.0/dist/dom-to-image.min.js"></script>
-<?php if ($state == 'de' or $state == 'nj') /* Version 3.0.10, with forked modifications */ { ?>
-    <script src="/asset/map/esri-leaflet.js"></script>
+<?php if ($state == 'de' or $state == 'nj') { ?>
+    <script src="/<?= ($online ? '/unpkg.com/esri-leaflet@3.0.11/dist' : 'asset/map') ?>/esri-leaflet.js"></script>
 <?php } ?>
 <script src="/<?= \Config\Services::request()->getLocale() ?>/<?= $state ?>/leaflet/"></script>
 <script src="/asset/map/leaflet-start.js"></script>
