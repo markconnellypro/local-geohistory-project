ece9a549b72eb71829fb69d323693d2b56b8421d
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-30 16:25:44 -0400

    Revert "Esri Leaflet 3.0.10 (modified) -> 3.0.11"
    This reverts commit cd2cf7018c13c2f7fdbd29822d070a48ac18665c.

--- src/app/Views/leaflet_start.php
+++ src/app/Views/leaflet_start.php
@@ -6,8 +6,8 @@ if (file_exists(APPPATH . 'Views/' . ENVIRONMENT . '/leaflet_rotation.php') AND 
 <script src="/<?= ($online ? '/unpkg.com/leaflet-fullscreen@1.0.2/dist' : 'asset/map') ?>/Leaflet.fullscreen.min.js"></script>
 <link rel="stylesheet" href="/<?= ($online ? '/unpkg.com/leaflet-fullscreen@1.0.2/dist' : 'asset/map') ?>/leaflet.fullscreen.css">
 <script src="/<?= ($online ? '/unpkg.com/dom-to-image@2.6.0/dist' : 'asset/map') ?>/dom-to-image.min.js"></script>
-<?php if ($state == 'de' or $state == 'nj') { ?>
-    <script src="/<?= ($online ? '/unpkg.com/esri-leaflet@3.0.11/dist' : 'asset/map') ?>/esri-leaflet.js"></script>
+<?php if ($state == 'de' or $state == 'nj') /* Version 3.0.10, with forked modifications */ { ?>
+    <script src="/asset/map/esri-leaflet.js"></script>
 <?php } ?>
 <script src="/<?= \Config\Services::request()->getLocale() ?>/<?= $state ?>/leaflet/"></script>
 <script src="/asset/map/leaflet-start.js"></script>
