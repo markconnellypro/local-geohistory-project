ceeaecb93267ecf2d67fea701cd70694d80da052
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2023-09-30 00:06:49 -0400

    Leaflet 1.9.3 -> 1.9.4

--- src/app/Views/leaflet_source.php
+++ src/app/Views/leaflet_source.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="/<?= ($online ? '/unpkg.com/leaflet@1.9.3/dist' : 'asset/map') ?>/leaflet.css" crossorigin="anonymous">
-<script src="/<?= ($online ? '/unpkg.com/leaflet@1.9.3/dist' : 'asset/map') ?>/leaflet.js"></script>
+<link rel="stylesheet" href="/<?= ($online ? '/unpkg.com/leaflet@1.9.4/dist' : 'asset/map') ?>/leaflet.css" crossorigin="anonymous">
+<script src="/<?= ($online ? '/unpkg.com/leaflet@1.9.4/dist' : 'asset/map') ?>/leaflet.js"></script>
 <script src="/<?= ($online ? '/unpkg.com/maplibre-gl@3.0.0/dist' : 'asset/map') ?>/maplibre-gl.js"></script>
 <script src="/<?= ($online ? '/unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.19' : 'asset/map') ?>/leaflet-maplibre-gl.js"></script>
 <link rel="stylesheet" href="/<?= ($online ? '/unpkg.com/maplibre-gl@3.0.0/dist' : 'asset/map') ?>/maplibre-gl.css" crossorigin="anonymous">
