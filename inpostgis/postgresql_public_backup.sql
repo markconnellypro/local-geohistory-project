d73cb979ac681ed999eca6c395b73935a99fdb5b
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-02-17 23:22:12 -0500

    Bump pg_dump version

--- inpostgis/postgresql_public_backup.sql
+++ inpostgis/postgresql_public_backup.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 16.1 (Debian 16.1-1.pgdg110+1)
--- Dumped by pg_dump version 16.1 (Ubuntu 16.1-1.pgdg22.04+1)
+-- Dumped by pg_dump version 16.2 (Ubuntu 16.2-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
