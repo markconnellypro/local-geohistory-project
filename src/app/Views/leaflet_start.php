cb49365a73f83b670897feaf27ed8d0bed54af66
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-03-01 04:02:02 -0500

    Fix dependency punctuation

--- src/app/Views/leaflet_start.php
+++ src/app/Views/leaflet_start.php
@@ -3,8 +3,8 @@ echo view('leaflet_source');
 if (file_exists(APPPATH . 'Views/' . ENVIRONMENT . '/leaflet_rotation.php') AND $needRotation) {
     echo view(ENVIRONMENT . '/leaflet_rotation');
 } ?>
-<script src="/<?= ($online ? '/' . getenv('dependency_leaflet-fullscreen') : 'asset/dependency') ?>/Leaflet.fullscreen.min.js"></script>
-<link rel="stylesheet" href="/<?= ($online ? '/' . getenv('dependency_leaflet-fullscreen') : 'asset/dependency') ?>/leaflet.fullscreen.css">
+<script src="/<?= ($online ? '/' . getenv('dependency_leaflet_fullscreen') : 'asset/dependency') ?>/Leaflet.fullscreen.min.js"></script>
+<link rel="stylesheet" href="/<?= ($online ? '/' . getenv('dependency_leaflet_fullscreen') : 'asset/dependency') ?>/leaflet.fullscreen.css">
 <script src="/<?= ($online ? '/' . getenv('dependency_html_to_image') : 'asset/dependency') ?>/html-to-image.js"></script>
 <?php if ($state == 'de' or $state == 'nj') /* Version 3.0.10, with forked modifications */ { ?>
     <script src="/asset/map/esri-leaflet.js"></script>
