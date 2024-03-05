75910132aacbbf90fe3b1b978047a88fac538477
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-03-05 00:31:38 -0500

    Add type hinting to Filter

--- src/app/Filters/LanguageRedirect.php
+++ src/app/Filters/LanguageRedirect.php
@@ -2,13 +2,14 @@
 
 namespace App\Filters;
 
+use CodeIgniter\HTTP\RedirectResponse;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
 class LanguageRedirect implements FilterInterface
 {
-    public function before(RequestInterface $request, $arguments = null)
+    public function before(RequestInterface $request, $arguments = null): null|RedirectResponse
     {
         $segments = $request->uri->getSegments();
         $locale = $request->getLocale();
@@ -17,9 +18,10 @@ class LanguageRedirect implements FilterInterface
             $segments = '/' . implode('/', $segments) . '/';
             return redirect()->to($segments);
         }
+        return null;
     }
 
-    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null): void
     {
     }
 }
