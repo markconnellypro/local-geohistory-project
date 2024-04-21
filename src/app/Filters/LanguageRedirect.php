2c3d922dfbd9787e88554f50ad476c6c5e470156
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-04-21 13:32:00 -0400

    Use header instead of CodeIgniter redirect->to in LanguageRedirect filter

--- src/app/Filters/LanguageRedirect.php
+++ src/app/Filters/LanguageRedirect.php
@@ -2,23 +2,22 @@
 
 namespace App\Filters;
 
-use CodeIgniter\HTTP\RedirectResponse;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
 class LanguageRedirect implements FilterInterface
 {
-    public function before(RequestInterface $request, $arguments = null): null|RedirectResponse
+    public function before(RequestInterface $request, $arguments = null): void
     {
         $segments = $request->getUri()->getSegments();
         $locale = $request->getLocale();
         if (isset($segments[0]) && $segments[0] !== $locale && $segments !== ['robots.txt']) {
             $segments[0] = $locale;
             $segments = '/' . implode('/', $segments) . '/';
-            return redirect()->to($segments);
+            header('Location: ' . $segments);
+            die();
         }
-        return null;
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null): void
