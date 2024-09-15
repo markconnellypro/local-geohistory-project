ae2b729915d224856ee938da6d4cd94436146650
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-09-15 13:31:04 -0400

    LanguageRedirect: Refactor for RequestInterface objects without getLocale method

--- src/app/Filters/LanguageRedirect.php
+++ src/app/Filters/LanguageRedirect.php
@@ -11,7 +11,7 @@ class LanguageRedirect implements FilterInterface
     public function before(RequestInterface $request, $arguments = null): void
     {
         $segments = $request->getUri()->getSegments();
-        $locale = $request->getLocale();
+        $locale = method_exists($request, 'getLocale') ? $request->getLocale() : 'en';
         if (isset($segments[0]) && $segments[0] !== $locale && $segments !== ['robots.txt']) {
             $segments[0] = $locale;
             $segments = '/' . implode('/', $segments) . '/';
