738e69978ef997e8965278faa7afb9fb6126b250
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-10-20 11:26:38 -0400

    Prepare routes for reformatting to remove most jurisdiction references

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -9,58 +9,49 @@ use CodeIgniter\Router\RouteCollection;
 $routes->add('robots.txt', 'Bot::robotsTxt');
 
 if (mb_strpos(base_url(), $_ENV['app_baseLocalGeohistoryProjectUrl']) !== false) {
-    if (ENVIRONMENT === 'development') {
-        // Includes all US and Canadian provinces, states, and territories
-        $stateProvinceRegex = '{locale}/(a[bklrsz]|bc|c[aot]|d[ce]|f[l]|g[au]|h[i]|i[adln]|k[sy]|l[a]|m[abdeinopst]|n[bcdehjlmstuvy]|o[hknr]|p[aer]|qc|r[i]|s[cdk]|t[nx]|u[t]|v[ait]|w[aivy]|yt)';
-
-        // Adds USA and CAN
-        $nationStateProvinceRegex = '{locale}/(a[bklrsz]|bc|c[aot]|can|d[ce]|f[l]|g[au]|h[i]|i[adln]|k[sy]|l[a]|m[abdeinost]|n[bcdehjlmstuvy]|o[hknr]|p[aer]|qc|r[i]|s[cdk]|t[nx]|u[t]|usa|v[ait]|w[aivy]|yt)';
-
-        // Temporary
-        $stateProvinceRegex = $nationStateProvinceRegex;
+    if ($_ENV['app_jurisdiction'] !== '') {
+        $stateProvinceRegex = '('. $_ENV['app_jurisdiction'] . ')';
     } else {
-        // Only selected states
-        $stateProvinceRegex = '{locale}/('. $_ENV['app_jurisdiction'] . ')';
-        $nationStateProvinceRegex = '{locale}/('. $_ENV['app_jurisdiction'] . ')';
+        $stateProvinceRegex = '(:segment)';
     }
 
     $controllerRegex = ['adjudication', 'area', 'event', 'government', 'governmentsource', 'law', 'metes', 'reporter', 'source'];
     $mainSearchRegex = '(event|government|adjudication|law)';
 
     foreach ($controllerRegex as $c) {
-        $routes->add($stateProvinceRegex . '/' . $c . '/(:segment)', ucwords($c) . '::view/$1/$2');
-        $routes->add($stateProvinceRegex . '/' . $c, ucwords($c) . '::noRecord/$1');
+        $routes->add('{locale}/' . $stateProvinceRegex . '/' . $c . '/(:segment)', ucwords($c) . '::view/$1/$2');
+        $routes->add('{locale}/' . $stateProvinceRegex . '/' . $c, ucwords($c) . '::noRecord/$1');
     }
 
-    $routes->add($stateProvinceRegex, 'Search::index/$1');
-    $routes->add($stateProvinceRegex . '/search/' . $mainSearchRegex, 'Search::view/$1/$2');
-    $routes->add($stateProvinceRegex . '/search/(:segment)', 'Search::noRecord/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex, 'Search::index/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/search/' . $mainSearchRegex, 'Search::view/$1/$2');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/search/(:segment)', 'Search::noRecord/$1');
 
-    $routes->add($stateProvinceRegex . '/address', 'Area::address/$1');
-    $routes->add($stateProvinceRegex . '/point/(:segment)/(:segment)', 'Area::point/$1/$2/$3');
-    $routes->add($stateProvinceRegex . '/point', 'Area::point/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/address', 'Area::address/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/point/(:segment)/(:segment)', 'Area::point/$1/$2/$3');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/point', 'Area::point/$1');
 
     $routes->add('{locale}/governmentidentifier/(:segment)/(:segment)', 'Governmentidentifier::view/$1/$2');
 
-    $routes->add($stateProvinceRegex . '/leaflet', 'Map::leaflet/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/leaflet', 'Map::leaflet/$1');
 
-    $routes->add($stateProvinceRegex . '/about', 'About::index/$1');
-    $routes->add($stateProvinceRegex . '/map-base', 'Map::baseStyle');
-    $routes->add($stateProvinceRegex . '/map-overlay', 'Map::overlayStyle/$1');
-    $routes->add($stateProvinceRegex . '/map-tile/(:num)/(:num)/(:num)', 'Map::tile/$2/$3/$4/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/about', 'About::index/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/map-base', 'Map::baseStyle');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/map-overlay', 'Map::overlayStyle/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/map-tile/(:num)/(:num)/(:num)', 'Map::tile/$2/$3/$4/$1');
     $routes->add('{locale}/about', 'About::index');
     $routes->add('{locale}/bot', 'Bot::index');
     $routes->add('{locale}/disclaimer', 'Disclaimer');
     $routes->add('{locale}/key', 'Key::index');
 
-    $routes->add($stateProvinceRegex . '/statistics/report/', 'Statistics::view/$1');
-    $routes->add($stateProvinceRegex . '/statistics/', 'Statistics::index/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/statistics/report/', 'Statistics::view/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/statistics/', 'Statistics::index/$1');
     $routes->add('{locale}/statistics/report/', 'Statistics::view');
     $routes->add('{locale}/statistics/', 'Statistics::index');
 
-    $routes->add($stateProvinceRegex . '/lookup/government/(:segment)', 'Search::governmentlookup/$1/$2/government');
-    $routes->add($stateProvinceRegex . '/lookup/governmentparent/(:segment)', 'Search::governmentlookup/$1/$2/governmentparent');
-    $routes->add($stateProvinceRegex . '/lookup/tribunal/(:num)', 'Search::tribunallookup/$1');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/lookup/government/(:segment)', 'Search::governmentlookup/$1/$2/government');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/lookup/governmentparent/(:segment)', 'Search::governmentlookup/$1/$2/governmentparent');
+    $routes->add('{locale}/' . $stateProvinceRegex . '/lookup/tribunal/(:num)', 'Search::tribunallookup/$1');
 }
 
 /*
