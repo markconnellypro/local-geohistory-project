3244ce1222430b85a23dc25fa2f81e61e29add53
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-10-30 22:58:46 -0400

    Change extra.giscountycache, extra.gismunicipalitycache, extra.gissubmunicipalitycache to gis.governmentshapecache

--- src/app/Models/GovernmentShapeModel.php
+++ src/app/Models/GovernmentShapeModel.php
@@ -480,45 +480,37 @@ class GovernmentShapeModel extends BaseModel
 
     // extra.ci_model_map_tile(v_state character varying, v_z integer, v_x integer, v_y integer)
 
-    // FUNCTION: extra.governmentshort
-    // VIEW: extra.giscountycache
-    // VIEW: extra.gismunicipalitycache
-    // VIEW: extra.gissubmunicipalitycache
-
     public function getTile(float $z, float $x, float $y): array
     {
         $query = <<<QUERY
-            WITH mvtgeometrycounty AS (
-                SELECT ST_AsMVTGeom(ST_Transform(giscountycache.geometry, 3857), ST_TileEnvelope(?, ?, ?), extent => 4096, buffer => 64) AS geometry,
-                giscountycache.government AS name,
-                extra.governmentshort(giscountycache.government) AS description
-                FROM extra.giscountycache
-                WHERE giscountycache.geometry && ST_Transform(ST_TileEnvelope(?, ?, ?, margin => (64.0 / 4096)), 4326)
-            ),
-            mvtgeometrymunicipality AS (
-                SELECT ST_AsMVTGeom(ST_Transform(gismunicipalitycache.geometry, 3857), ST_TileEnvelope(?, ?, ?), extent => 4096, buffer => 64) AS geometry,
-                gismunicipalitycache.government AS name,
-                extra.governmentshort(gismunicipalitycache.government) AS description
-                FROM extra.gismunicipalitycache
-                WHERE ? >= 6 
-                AND gismunicipalitycache.geometry && ST_Transform(ST_TileEnvelope(?, ?, ?, margin => (64.0 / 4096)), 4326)
-            ),
-            mvtgeometrysubmunicipality AS (
-                SELECT ST_AsMVTGeom(ST_Transform(gissubmunicipalitycache.geometry, 3857), ST_TileEnvelope(?, ?, ?), extent => 4096, buffer => 64) AS geometry,
-                gissubmunicipalitycache.government AS name,
-                extra.governmentshort(gissubmunicipalitycache.government) AS description
-                FROM extra.gissubmunicipalitycache
-                WHERE ? >= 6 
-                AND gissubmunicipalitycache.geometry && ST_Transform(ST_TileEnvelope(?, ?, ?, margin => (64.0 / 4096)), 4326)
+            WITH mvtgeometry AS (
+                SELECT ST_AsMVTGeom(ST_Transform(governmentshapecache.geometry, 3857), ST_TileEnvelope(?, ?, ?), extent => 4096, buffer => 64) AS geometry,
+                    government.governmentid AS name,
+                    government.governmentshort AS description,
+                    governmentshapecache.governmentlayer
+                FROM gis.governmentshapecache
+                JOIN geohistory.government
+                    ON governmentshapecache.government = government.governmentid
+                WHERE (
+                    governmentshapecache.governmentlayer = 'county'
+                    OR (
+                        governmentshapecache.governmentlayer IN ('municipality', 'submunicipality')
+                        AND (? >= 6)
+                    )
+                )
+                AND governmentshapecache.geometry && ST_Transform(ST_TileEnvelope(?, ?, ?, margin => (64.0 / 4096)), 4326)
             )
-            SELECT ST_AsMVT(mvtgeometrycounty.*, 'county') AS mvt
-            FROM mvtgeometrycounty
+            SELECT ST_AsMVT(mvtgeometry.*, mvtgeometry.governmentlayer) AS mvt
+            FROM mvtgeometry
+            WHERE mvtgeometry.governmentlayer = 'county'
             UNION
-            SELECT ST_AsMVT(mvtgeometrymunicipality.*, 'municipality') AS mvt
-            FROM mvtgeometrymunicipality
+            SELECT ST_AsMVT(mvtgeometry.*, mvtgeometry.governmentlayer) AS mvt
+            FROM mvtgeometry
+            WHERE mvtgeometry.governmentlayer = 'municipality'
             UNION
-            SELECT ST_AsMVT(mvtgeometrysubmunicipality.*, 'submunicipality') AS mvt
-            FROM mvtgeometrysubmunicipality;
+            SELECT ST_AsMVT(mvtgeometry.*, mvtgeometry.governmentlayer) AS mvt
+            FROM mvtgeometry
+            WHERE mvtgeometry.governmentlayer = 'submunicipality';
         QUERY;
 
         $query = $this->db->query($query, [
@@ -526,22 +518,9 @@ class GovernmentShapeModel extends BaseModel
             $x,
             $y,
             $z,
-            $x,
-            $y,
             $z,
             $x,
-            $y,
-            $z,
-            $z,
-            $x,
-            $y,
-            $z,
-            $x,
-            $y,
-            $z,
-            $z,
-            $x,
-            $y,
+            $y,         
         ]);
 
         return $this->getObject($query);
