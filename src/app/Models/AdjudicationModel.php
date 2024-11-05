4fc55da6a526d3a8203cd8d42efc348b2f168b20
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-11-04 20:27:00 -0500

    Reformat AdjudicationModel

--- src/app/Models/AdjudicationModel.php
+++ src/app/Models/AdjudicationModel.php
@@ -3,14 +3,56 @@
 namespace App\Models;
 
 use App\Models\BaseModel;
+use App\Models\TribunalModel;
 
 class AdjudicationModel extends BaseModel
 {
+    private function getFields(): string
+    {
+        $TribunalModel = new TribunalModel();
+
+        return <<<QUERY
+            SELECT DISTINCT adjudication.adjudicationid,
+                adjudication.adjudicationslug,
+                adjudicationtype.adjudicationtypelong,
+                adjudication.adjudicationnumber,
+                calendar.historicdatetextformat((adjudication.adjudicationterm || CASE
+                    WHEN length(adjudication.adjudicationterm) = 4 THEN '-~07-~28'
+                    WHEN length(adjudication.adjudicationterm) = 7 THEN '-~28'
+                    ELSE ''
+                END)::calendar.historicdate, 'short', ?) AS adjudicationterm,
+                adjudication.adjudicationterm AS adjudicationtermsort,
+                adjudication.adjudicationlong,
+                adjudication.adjudicationshort,
+                adjudication.adjudicationnotes,
+                adjudication.adjudicationtitle,
+                extra.tribunalfilingoffice(tribunal.tribunalid) AS tribunalfilingoffice,
+        QUERY . $TribunalModel->getLong() . <<<QUERY
+                AS tribunallong,
+        QUERY . $TribunalModel->getFilingOffice() . <<<QUERY
+                AS tribunalfilingoffice
+        QUERY;
+    }
+
+    private function getTables(): string
+    {
+        return <<<QUERY
+            FROM geohistory.adjudication
+            JOIN geohistory.adjudicationtype
+                ON adjudication.adjudicationtype = adjudicationtype.adjudicationtypeid
+            JOIN geohistory.tribunal
+                ON adjudicationtype.tribunal = tribunal.tribunalid
+            JOIN geohistory.tribunaltype
+                ON tribunal.tribunaltype = tribunaltype.tribunaltypeid
+            JOIN geohistory.government
+                ON tribunal.government = government.governmentid
+            JOIN geohistory.government governmentstate
+                ON government.governmentcurrentleadstateid = governmentstate.governmentid
+        QUERY;
+    }
+
     // extra.ci_model_adjudication_detail(character varying, character varying)
     // extra.ci_model_adjudication_detail(integer, character varying)
-
-    // FUNCTION: extra.tribunalfilingoffice
-    // FUNCTION: extra.tribunallong
 
     public function getDetail(int|string $id): array
     {
@@ -18,31 +60,13 @@ class AdjudicationModel extends BaseModel
             $id = $this->getSlugId($id);
         }
 
-        $query = <<<QUERY
-            SELECT DISTINCT adjudication.adjudicationid,
-                adjudicationtype.adjudicationtypelong,
-                extra.tribunallong(tribunal.tribunalid) AS tribunallong,
-                adjudication.adjudicationnumber,
-                calendar.historicdatetextformat((adjudication.adjudicationterm || CASE
-                    WHEN length(adjudication.adjudicationterm) = 4 THEN '-~07-~28'
-                    WHEN length(adjudication.adjudicationterm) = 7 THEN '-~28'
-                    ELSE ''
-                END)::calendar.historicdate, 'short', ?) AS adjudicationterm,
-                CASE
+        $query = $this->getFields() . <<<QUERY
+                , CASE
                     WHEN adjudication.adjudicationlong = '' AND adjudication.adjudicationshort = '' AND adjudication.adjudicationnotes = '' THEN FALSE
                     ELSE TRUE
-                END AS textflag,
-                adjudication.adjudicationlong,
-                adjudication.adjudicationshort,
-                adjudication.adjudicationnotes,
-                extra.tribunalfilingoffice(tribunal.tribunalid) AS tribunalfilingoffice,
-                adjudication.adjudicationtitle
-            FROM geohistory.adjudicationtype
-            JOIN geohistory.tribunal
-                ON adjudicationtype.tribunal = tribunal.tribunalid
-            JOIN geohistory.adjudication
-                ON adjudicationtype.adjudicationtypeid = adjudication.adjudicationtype
-                AND adjudication.adjudicationid = ?
+                END AS textflag
+        QUERY . $this->getTables() . <<<QUERY
+            WHERE adjudication.adjudicationid = ?
         QUERY;
 
         $query = $this->db->query($query, [
@@ -55,24 +79,9 @@ class AdjudicationModel extends BaseModel
 
     // extra.ci_model_reporter_adjudication(integer)
 
-    // FUNCTION: extra.tribunallong
-
     public function getByAdjudicationSourceCitation(int $id): array
     {
-        $query = <<<QUERY
-            SELECT adjudication.adjudicationslug,
-                adjudicationtype.adjudicationtypelong,
-                extra.tribunallong(adjudicationtype.tribunal) AS tribunallong,
-                adjudication.adjudicationnumber,
-                calendar.historicdatetextformat((adjudication.adjudicationterm || CASE
-                    WHEN length(adjudication.adjudicationterm) = 4 THEN '-~07-~28'
-                    WHEN length(adjudication.adjudicationterm) = 7 THEN '-~28'
-                    ELSE ''
-                END)::calendar.historicdate, 'short', ?) AS adjudicationterm,
-                adjudication.adjudicationterm AS adjudicationtermsort
-            FROM geohistory.adjudication
-            JOIN geohistory.adjudicationtype
-                ON adjudication.adjudicationtype = adjudicationtype.adjudicationtypeid
+        $query = $this->getFields() . $this->getTables() . <<<QUERY
             JOIN geohistory.adjudicationsourcecitation
                 ON adjudication.adjudicationid = adjudicationsourcecitation.adjudication
                 AND adjudicationsourcecitation.adjudicationsourcecitationid = ?
@@ -88,30 +97,16 @@ class AdjudicationModel extends BaseModel
 
     // extra.ci_model_event_adjudication(integer)
 
-    // FUNCTION: extra.tribunallong
-
     public function getByEvent(int $id): array
     {
-        $query = <<<QUERY
-            SELECT adjudication.adjudicationslug,
-                adjudicationtype.adjudicationtypelong,
-                extra.tribunallong(adjudicationtype.tribunal) AS tribunallong,
-                adjudication.adjudicationnumber,
-                calendar.historicdatetextformat((adjudication.adjudicationterm || CASE
-                    WHEN length(adjudication.adjudicationterm) = 4 THEN '-~07-~28'
-                    WHEN length(adjudication.adjudicationterm) = 7 THEN '-~28'
-                    ELSE ''
-                END)::calendar.historicdate, 'short', ?) AS adjudicationterm,
-                adjudication.adjudicationterm AS adjudicationtermsort,
-                eventrelationship.eventrelationshipshort AS eventrelationship
-            FROM geohistory.adjudicationevent
+        $query = $this->getFields() . <<<QUERY
+                , eventrelationship.eventrelationshipshort AS eventrelationship
+        QUERY . $this->getTables() . <<<QUERY
+            JOIN geohistory.adjudicationevent
+                ON adjudication.adjudicationid = adjudicationevent.adjudication
+                AND adjudicationevent.event = ?
             JOIN geohistory.eventrelationship
                 ON adjudicationevent.eventrelationship = eventrelationship.eventrelationshipid
-            JOIN geohistory.adjudication
-                ON adjudicationevent.adjudication = adjudication.adjudicationid
-            JOIN geohistory.adjudicationtype
-                ON adjudication.adjudicationtype = adjudicationtype.adjudicationtypeid
-            WHERE adjudicationevent.event = ?
         QUERY;
 
         $query = $this->db->query($query, [
