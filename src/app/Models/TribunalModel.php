bb20d81ae16160ef9b10363a771878ed96d29979
Mark A. Connelly <16519297+markconnellypro@users.noreply.github.com>
2024-12-17 21:35:37 -0500

    TribunalModel->getLong: Reorganize; fix errors

--- src/app/Models/TribunalModel.php
+++ src/app/Models/TribunalModel.php
@@ -26,43 +26,60 @@ class TribunalModel extends BaseModel
         return <<<QUERY
                 tribunaltype.tribunaltypelong || ' of ' ||
                 CASE
-                    WHEN tribunaltype.tribunaltypelevel > 2 THEN government.governmentshortshort || ', ' || governmentstate.governmentshortshort ||
-                    CASE
-                        WHEN tribunaltype.tribunaltypedivision <> '' THEN ' - ' || tribunaltype.tribunaltypedivision
-                        ELSE ''
-                    END
-                    WHEN tribunaltype.tribunaltypelevel = 2 THEN government.governmentshortshort ||
-                    CASE
-                        WHEN tribunal.tribunaldistrictcircuit = 'E' THEN ' - Eastern District'
-                        WHEN tribunal.tribunaldistrictcircuit = 'M' THEN ' - Middle District'
-                        WHEN tribunal.tribunaldistrictcircuit = 'W' THEN ' - Western District'
-                        WHEN tribunal.tribunaldistrictcircuit = 'N' THEN ' - Northern District'
-                        WHEN tribunal.tribunaldistrictcircuit = 'S' THEN ' - Southern District'
-                        WHEN tribunal.tribunaldistrictcircuit = 'C' THEN ' - Central District'
-                        ELSE ''
-                    END
-                    WHEN tribunaltype.tribunaltypelevel = 1 THEN
-                    CASE
-                        WHEN tribunal.tribunaldistrictcircuit = 'E' THEN 'the Eastern District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = 'M' THEN 'the Middle District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = 'W' THEN 'the Western District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = 'N' THEN 'the Northern District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = 'S' THEN 'the Southern District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = 'C' THEN 'the Central District of ' || government.governmentshortshort
-                        WHEN tribunal.tribunaldistrictcircuit = '1' THEN 'the First Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '2' THEN 'the Second Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '3' THEN 'the Third Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '4' THEN 'the Fourth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '5' THEN 'the Fifth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '6' THEN 'the Sixth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '7' THEN 'the Seventh Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '8' THEN 'the Eighth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '9' THEN 'the Ninth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '10' THEN 'the Tenth Circuit'
-                        WHEN tribunal.tribunaldistrictcircuit = '11' THEN 'the Eleventh Circuit'
-                        WHEN government.governmentlevel = 1 OR government.governmentname = 'District of Columbia' THEN 'the ' || government.governmentshortshort
-                        ELSE government.governmentshortshort
-                    END
+                    WHEN tribunaltype.tribunaltypelevel > 2 THEN government.governmentshortshort || ', ' || governmentstate.governmentshortshort
+                    WHEN tribunaltype.tribunaltypelevel = 2 THEN government.governmentshortshort
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunaltype.tribunaltypedivision <> '' THEN ', ' || tribunaltype.tribunaltypedivision
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunaltype.tribunaltypelevel = 1 AND tribunal.tribunaldistrictcircuit <> '' THEN 'the '
+                    WHEN tribunal.tribunaldistrictcircuit <> '' THEN ', '
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunal.tribunaldistrictcircuit = '1' THEN 'First'
+                    WHEN tribunal.tribunaldistrictcircuit = '2' THEN 'Second'
+                    WHEN tribunal.tribunaldistrictcircuit = '3' THEN 'Third'
+                    WHEN tribunal.tribunaldistrictcircuit = '4' THEN 'Fourth'
+                    WHEN tribunal.tribunaldistrictcircuit = '5' THEN 'Fifth'
+                    WHEN tribunal.tribunaldistrictcircuit = '6' THEN 'Sixth'
+                    WHEN tribunal.tribunaldistrictcircuit = '7' THEN 'Seventh'
+                    WHEN tribunal.tribunaldistrictcircuit = '8' THEN 'Eighth'
+                    WHEN tribunal.tribunaldistrictcircuit = '9' THEN 'Ninth'
+                    WHEN tribunal.tribunaldistrictcircuit = '10' THEN 'Tenth'
+                    WHEN tribunal.tribunaldistrictcircuit = '11' THEN 'Eleventh'
+                    WHEN tribunal.tribunaldistrictcircuit = '12' THEN 'Twelfth'
+                    WHEN tribunal.tribunaldistrictcircuit = '13' THEN 'Thirteenth'
+                    WHEN tribunal.tribunaldistrictcircuit = '14' THEN 'Fourteenth'
+                    WHEN tribunal.tribunaldistrictcircuit = 'C' THEN 'Central'
+                    WHEN tribunal.tribunaldistrictcircuit = 'E' THEN 'Eastern'
+                    WHEN tribunal.tribunaldistrictcircuit = 'M' THEN 'Middle'
+                    WHEN tribunal.tribunaldistrictcircuit = 'N' THEN 'Northern' 
+                    WHEN tribunal.tribunaldistrictcircuit = 'Rich' THEN 'Richmond'
+                    WHEN tribunal.tribunaldistrictcircuit = 'S' THEN 'Southern'
+                    WHEN tribunal.tribunaldistrictcircuit = 'Scrtn' THEN 'Scranton'
+                    WHEN tribunal.tribunaldistrictcircuit = 'Sta' THEN 'Staunton'
+                    WHEN tribunal.tribunaldistrictcircuit = 'W' THEN 'Western'
+                    WHEN tribunal.tribunaldistrictcircuit = 'W.Lyn' THEN 'Western'
+                    WHEN tribunal.tribunaldistrictcircuit = 'Wmpt' THEN 'Williamsport'
+                    WHEN tribunal.tribunaldistrictcircuit = 'Wyt' THEN 'Wytheville'
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunal.tribunaldistrictcircuit <> '' THEN ' ' || tribunaltype.tribunaltypedistrictcircuit
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunaltype.tribunaltypelevel = 1 AND (tribunaltype.tribunaltypelong = 'Supreme Court' OR government.governmentname = 'Columbia') THEN 'the ' || government.governmentshortshort
+                    WHEN tribunaltype.tribunaltypelevel = 1 AND tribunaltype.tribunaltypedistrictcircuit = 'District' AND tribunal.tribunaldistrictcircuit = '' THEN government.governmentshortshort
+                    WHEN tribunaltype.tribunaltypelevel = 1 AND tribunaltype.tribunaltypedistrictcircuit = 'District' THEN ' of ' || government.governmentshortshort
+                    ELSE ''
+                END ||
+                CASE
+                    WHEN tribunal.tribunaldistrictcircuit = 'W.Lyn' THEN ', Lynchburg Division'
                     ELSE ''
                 END
             QUERY;
